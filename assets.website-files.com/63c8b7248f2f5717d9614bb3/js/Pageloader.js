721c82c284895b819c07a3283bbacac6eb769b27
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2023-10-19 11:54:40 -0400

    Removed some code fro mpage loader that wasn't needed

--- assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
+++ assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
@@ -4,10 +4,3 @@ window.onload = function() {
   $loader.classList.remove('loader--active')
 };
 
-document.querySelector('.btn').addEventListener('click', function () {
-  $loader.classList.add('loader--active')
-  
-  window.setTimeout(function () {
-    $loader.classList.remove('loader--active')
-  }, 5000)
-})
