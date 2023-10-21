489bd830dd2753429b427a4833d9f9c39e036bdd
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2023-10-20 20:40:13 -0400

    updated with the btn

--- assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
+++ assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
@@ -4,3 +4,10 @@ window.onload = function() {
   $loader.classList.remove('loader--active')
 };
 
+document.querySelector('.btn').addEventListener('click', function () {
+  $loader.classList.add('loader--active')
+  
+  window.setTimeout(function () {
+    $loader.classList.remove('loader--active')
+  }, 5000)
+})
