a482636d05e11f532ddb0cf781b748e2fd9ab1bb
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2023-10-21 15:50:14 -0400

    updated the page laoding functin

--- assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
+++ assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
@@ -1,20 +1,28 @@
-var $loader = document.querySelector('.loader');
+document.addEventListener('DOMContentLoaded', function() {
+  var $loader = document.querySelector('.loader');
+  if (!$loader) {
+    console.error('Loader element not found');
+    return;
+  }
 
-window.onload = function() {
   $loader.classList.remove('loader--active');
-};
 
-document.querySelectorAll('.btn').forEach(function(btn) {
-  btn.addEventListener('click', function (event) {
-    event.preventDefault(); // Prevent the default action (navigation)
-    $loader.classList.add('loader--active');
-    
-    // Get the href attribute of the parent <a> element
-    var href = this.closest('a').getAttribute('href');
-    
-    window.setTimeout(function () {
-      $loader.classList.remove('loader--active');
-      window.location.href = href; // Redirect after the animation
-    }, 2500); // Adjust the timeout to match your animation duration
+  document.querySelectorAll('.btn').forEach(function(btn) {
+    btn.addEventListener('click', function (event) {
+      event.preventDefault(); // Prevent the default action (navigation)
+      $loader.classList.add('loader--active');
+      
+      var anchor = this.closest('a');
+      if (!anchor) {
+        console.error('Anchor element not found');
+        return;
+      }
+      var href = anchor.getAttribute('href');
+      
+      window.setTimeout(function () {
+        $loader.classList.remove('loader--active');
+        window.location.href = href; // Redirect after the animation
+      }, 2500); // Adjust the timeout to match your animation duration
+    });
   });
 });
