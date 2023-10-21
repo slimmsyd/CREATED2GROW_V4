ac729ab0c453a8186ad0e2102d594342589ed66c
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2023-10-21 15:40:50 -0400

    updated the page loader

--- assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
+++ assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
@@ -1,13 +1,20 @@
-var $loader = document.querySelector('.loader')
+var $loader = document.querySelector('.loader');
 
 window.onload = function() {
-  $loader.classList.remove('loader--active')
+  $loader.classList.remove('loader--active');
 };
 
-document.querySelector('.btn').addEventListener('click', function () {
-  $loader.classList.add('loader--active')
-  
-  window.setTimeout(function () {
-    $loader.classList.remove('loader--active')
-  }, 5000)
-})
+document.querySelectorAll('.btn').forEach(function(btn) {
+  btn.addEventListener('click', function (event) {
+    event.preventDefault(); // Prevent the default action (navigation)
+    $loader.classList.add('loader--active');
+    
+    // Get the href attribute of the parent <a> element
+    var href = this.closest('a').getAttribute('href');
+    
+    window.setTimeout(function () {
+      $loader.classList.remove('loader--active');
+      window.location.href = href; // Redirect after the animation
+    }, 2500); // Adjust the timeout to match your animation duration
+  });
+});
