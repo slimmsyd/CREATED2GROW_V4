6fe8a823ea3d64c80abd4c231397a8b1cd5831a5
Sydney Sanders <84546691+slimmsyd@users.noreply.github.com>
2023-10-21 20:07:52 -0400

    reverted it back

--- assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
+++ assets.website-files.com/63c8b7248f2f5717d9614bb3/js/Pageloader.js
@@ -1,29 +1,30 @@
-$(document).ready(function() {
-  var $loader = $('.loader');
-  if ($loader.length === 0) {
+document.addEventListener('DOMContentLoaded', function() {
+  var $loader = document.querySelector('.loader');
+  if (!$loader) {
     console.error('Loader element not found');
     return;
   }
 
-  $loader.removeClass('loader--active');
+  $loader.classList.remove('loader--active');
 
-  $('.btn').on('click', function(event) {
-    console.log('Button clicked');
-    event.preventDefault(); // Prevent the default action (navigation)
-    console.log('Default action prevented');
-    $loader.addClass('loader--active');
-    
-    var $anchor = $(this).closest('a');
-    if ($anchor.length === 0) {
-      console.error('Anchor element not found');
-      return;
-    }
-    var href = $anchor.attr('href');
-    
-    setTimeout(function() {
-      console.log('Navigating to:', href);
-      $loader.removeClass('loader--active');
-      window.location.href = href; // Redirect after the animation
-    }, 2500); // Adjust the timeout to match your animation duration
+  document.querySelectorAll('.btn').forEach(function(btn) {
+    btn.addEventListener('click', function (event) {
+      console.log('Button clicked');
+      console.log('Default action prevented');
+      $loader.classList.add('loader--active');
+      
+      var anchor = this.closest('a');
+      if (!anchor) {
+        console.error('Anchor element not found');
+        return;
+      }
+      var href = anchor.getAttribute('href');
+      
+      window.setTimeout(function () {
+        console.log('Navigating to:', href);
+        $loader.classList.remove('loader--active');
+        window.location.href = href; // Redirect after the animation
+      }, 2500); // Adjust the timeout to match your animation duration
+    });
   });
 });
